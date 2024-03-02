98d604f49ba06635285dff20880658942bc67a85
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2024-03-01 23:18:28 -0300

    laços de repeti~cao e condicoes

---
+++ aula-11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 55;
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+if (vel > 60) { // condição simples
+    console.log(`você ultrapassou a velociade permitida e você esta multado`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
