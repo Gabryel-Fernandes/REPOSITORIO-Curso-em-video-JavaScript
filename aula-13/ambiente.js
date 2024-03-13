a3d60d16384017b6b0e76f8a829ebd42b8f1a9ef
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2024-03-12 23:27:48 -0300

    estruturas de repetição while e do while

---
+++ aula-13/ambiente.js
@@ -0,0 +1,21 @@
+var cont = 1;
+do {
+    console.log('tudo bem');
+    cont++;
+} while (cont < 6);
+
+var cont = 8;
+while(cont < 6){
+    console.log('tudo bem');
+    cont++;
+}
+if(cont > 6){
+    console.log('Seu número exede o valor maximo')
+}
+
+/*console.log('tudo bem');
+console.log('tudo bem');
+console.log('tudo bem');
+console.log('tudo bem');
+console.log('tudo bem');
+console.log('tudo bem');*/
---
+++ aula-13/teste.js
@@ -0,0 +1,6 @@
+var tiros = 1;
+
+do{
+    console.log('tiro de aviso');
+    tiros ++;
+}while(tiros < 10);
